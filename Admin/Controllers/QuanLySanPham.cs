38f440eb952d400552c8f4723e6bd05bfad17934
nguyentrungducgray <trungduc2004k54@gmail.com>
2024-10-16 22:00:54 +0700

    Admin

---
+++ Admin/Controllers/QuanLySanPham.cs
@@ -0,0 +1,55 @@
+﻿using Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Admin.Controllers
+{
+    public class QuanLySanPham : Controller
+    {
+        GreenCoderContext db = new GreenCoderContext();
+        [Route("QuanLySanPham")]
+        public IActionResult Index()
+        {
+            var lstSP = db.SanPhams.ToList();
+            return View(lstSP);
+        }
+
+        [Route("ThemSanPhamMoi")]
+        [HttpGet]
+        public IActionResult ThemSanPham()
+        {
+            return View();
+        }
+        [Route("ThemSanPhamMoi")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult ThemSanPham(SanPham SanPham)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                db.SanPhams.Add(SanPham);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(SanPham);
+        }
+
+        [HttpGet]
+        public IActionResult Update(int ID) {
+            var SanPham = db.SanPhams.Find(ID);
+            return View(SanPham);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(SanPham sanPham)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SanPhams.Update(sanPham);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sanPham);
+        }
+    }
+}
---
+++ Admin/Models/SanPham.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Admin.Models;
+
+public partial class SanPham
+{
+    public int Id { get; set; }
+
+    public string TenSanPham { get; set; } = null!;
+
+    public int SoLuong { get; set; }
+
+    public string? TinhTrang { get; set; }
+
+    public string? DanhMuc { get; set; }
+
+    public string? NhaCungCap { get; set; }
+
+    public int GiaBan { get; set; }
+
+    public int? GiaVon { get; set; }
+
+    public string? AnhSanPham { get; set; }
+
+    public string? MoTa { get; set; }
+
+    public virtual ICollection<DonHang> DonHangs { get; set; } = new List<DonHang>();
+}
