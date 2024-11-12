7a1202eda8b40bfd2b04ed821a36708d81be8cc8
Ha Ngoc Linh <49241285+harnetlinh@users.noreply.github.com>
2024-11-12 23:57:14 +0700

    Update Program.cs

--- Web_bestcoder/Program.cs
+++ Web_bestcoder/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting.WindowsServices;
 
 namespace Web_bestcoder
 {
